ebbd2b8d11871be0a46f66fb5ec921f01dcf9e45
Michael Bardsley <michael.bardsley@wrenliving.com>
2014-11-21 15:09:17 +0000

    added some new tests

--- run.php
+++ run.php
@@ -3,16 +3,11 @@
 include_once __DIR__ . '/vendor/autoload.php';
 
 use mic2100\benchmark\benchmarker;
-use mic2100\benchmark\tests\absNegativeToPositiveWithMultipleIterations;
-use mic2100\benchmark\tests\lTrimNegativeToPositiveWithMultipleIterations;
-use mic2100\benchmark\tests\operatorsNegativeToPositiveWithMultipleIterations;
-use mic2100\benchmark\tests\strReplaceNegativeToPositiveWithMultipleIterations;
+use mic2100\benchmark\tests;
 
 $benchmark = new benchmarker;
 
-$benchmark->add(new absNegativeToPositiveWithMultipleIterations)
-          ->add(new lTrimNegativeToPositiveWithMultipleIterations)
-          ->add(new operatorsNegativeToPositiveWithMultipleIterations)
-          ->add(new strReplaceNegativeToPositiveWithMultipleIterations);
+$benchmark->add(new tests\usingIntCastCheckWithMultipleIterations)
+          ->add(new tests\usingIntValCheckWithMultipleIterations);
 
 $benchmark->execute()->report();
